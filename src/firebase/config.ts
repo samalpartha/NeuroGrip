dc2a15ea9d8e25e03528ef1d0291f8c9a17129ae
Partha Samal <samalpartha@gmail.com>
2025-11-29 19:12:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3500812965-b10c2",
+  "appId": "1:27535192750:web:c664a208ac7573abd0b33f",
+  "apiKey": "AIzaSyBGS9F8tSG1JFmEvuIH7nOFXEf1sBonAZM",
+  "authDomain": "studio-3500812965-b10c2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "27535192750"
+};
